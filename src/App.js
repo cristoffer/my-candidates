13196674b0f3b562e98fd912b9627a57f0c5f4e5
cristoffer carlberg <cristoffer.carlberg@rebelandbird.com>
2022-02-27 22:34:31 +0100

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import CandidateList from './blocks/candidateList';
 function App() {
   return (
     <div className="app">
-      <header className="app__header">
-        
-      </header>
       <div className="app__body">
         <CandidateList />
       </div>
